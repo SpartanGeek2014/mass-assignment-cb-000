d4604cfd95a83de7ebb0d8c7a0d05d9b2c95d13c
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-11-29 15:58:46 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,14 +3,11 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size,
                 :wrist_size, :glove_size, :pant_length, :pant_width
 
-      def iniitalize(attributes)
-        attributes.each {|key, value| self.send(("#{key}="), value)}
-
-      susan_attributes = {name: "Susan", height: "5'11\"", eye_color: "Green"}
-
-      susan = Person.new(susan_attributes)
-      susan.name
-      susan.height
-      susan.eye_color
+      def iniitalize(attributes=nil)
+        if attributes
+          attributes.each do |k, v|
+            self.send("#{k}=", v)
+          end
+        end
     end
 end
